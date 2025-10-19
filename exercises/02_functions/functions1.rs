f5a8467233850b1057926f86028b2a8d634a13c1
Baily <blitheshuo@gmail.com>
2025-10-18 22:45:08 -0400

    2025-10 functions part finished

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,9 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
+fn call_me() {
+    println!("Say Hello!");
+} // Don't change this line
+
 fn main() {
-    call_me(); // Don't change this line
+    call_me();
 }
