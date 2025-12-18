6241cae84a2ef87bb179cdabee9dbd7bfb44aed6
Baily <blitheshuo@gmail.com>
2025-12-18 00:05:30 -0500

    2025-12 iterator refreshing

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -28,7 +28,7 @@ fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `Ok([1, 11, 1426, 3])`
-fn result_with_list() {
+fn result_with_list() -> Result<Vec<i64>, DivisionError> {
     let numbers = [27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
     division_results.collect()
@@ -36,7 +36,7 @@ fn result_with_list() {
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
-fn list_of_results() {
+fn list_of_results() -> Vec<Result<i64, DivisionError>> {
     let numbers = [27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
     division_results.collect()
